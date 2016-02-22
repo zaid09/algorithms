e5caf2953c09a3bd932cbf615cb3a6922e19b3ba
zaid09 <analchoudhury1@gmail.com>
2016-02-22 21:56:18 +0530

    Counting Sort

---
+++ ug_course/cSort.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#define RANGE 255
+ 
+void countSort(char *str)
+{
+
+    char output[strlen(str)];
+ 
+    int count[RANGE + 1], i;
+    memset(count, 0, sizeof(count));
+
+    for(i = 0; str[i]; ++i)
+        ++count[str[i]];
+
+    for (i = 1; i <= RANGE; ++i)
+        count[i] += count[i-1];
+
+    for (i = 0; str[i]; ++i)
+    {
+        output[count[str[i]]-1] = str[i];
+        --count[str[i]];
+    }
+
+    for (i = 0; str[i]; ++i)
+        str[i] = output[i];
+}
+
+int main()
+{
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s",str);
+    countSort(str);
+ 
+    printf("Sorted string is: %s\n", str);
+    return 0;
+}
